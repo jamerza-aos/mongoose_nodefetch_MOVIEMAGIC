d082f1eebef804b539a98f2b8518a45e8af66a17
aos-jamerza <awsjamerza@gmail.com>
2021-01-21 22:19:21 +0100

    favourite

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ mongoose.connect(process.env.dbUri, { useNewUrlParser: true, useUnifiedTopology:
     .catch(err => console.log(err))
   
 
-    // https://api.themoviedb.org/3/movie/550?api_key=4f2d4313669b932746a7cbe1b3fff187
 
     app.get('/', (req, res) => {  
 
@@ -34,6 +33,95 @@ mongoose.connect(process.env.dbUri, { useNewUrlParser: true, useUnifiedTopology:
     .catch(err => console.log(err))     
     })
 
+    app.get('/details/:id' , (req, res) =>{
+        //    console.log(req.params.id);
+            fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=f9c8e3bf33252c8c1179e8b6ca4946f1`)
+           
+            .then(res => res.json())
+            .then(json => {
+                // console.log(json)
+                res.render("details", { single: json })
+            })
+          .catch(err=>console.log(err))
+          })
+
+
+
+          app.get("/addfavourites/:id", (req, res) => {
+                    fetch(`https://api.themoviedb.org/3/movie/${req.params.id}?api_key=f9c8e3bf33252c8c1179e8b6ca4946f1`)
+                        .then(res => res.json())
+                        .then(json => {
+                            console.log(json);
+                            const data1 = json
+                            movie.find({ id: data1.id })
+                                .then(result => {
+                                    if (result.length > 0) {
+                                        res.redirect("/favourites")
+                                    } else {
+                                        const newmovieItems = new movie({
+                                            id: data1.id,
+                                            title: data1.title,
+                                            overview: data1.overview,
+                                            genres: data1.genres,
+                
+                                            poster_path: data1.poster_path,
+                                            backdrop_path: data1.backdrop_path,
+                
+                                            vote_average: data1.vote_average,
+                                            popularity: data1.popularity,
+                                            original_title: data1.original_title,
+                                            release_date: data1.release_date,
+                                            status: data1.status
+                                        })
+                                        newmovieItems
+                                            .save()
+                                            .then(result => {
+                                                res.redirect("/favourites")
+                                            })
+                                    }
+                                })
+                        })
+                })            
+
+
+
+      
+        
+        app.get("/favourites", (req, res) => {
+            movie.find()
+                .then(result => {
+                    res.render('favourites', { myMovies: result })
+                })
+                .catch(err => console.log(err))
+        })
+
+
+
+              app.get("/favourites", (req, res) => {
+        movie.find()
+            .then(result => {
+                res.render('favourites', { Movies: result })
+            })
+            .catch(err => console.log(err))
+    })
+
+app.get('/removeFavourite/:id',(req,res)=>{
+    movie.findById(req.params.id)
+    .then(result =>{
+        res.render('removeFavourite', {removedata:result})
+    })
+    .catch(err=>console.log(err))
+})   
+
+
+app.get("/removefavourite/:id/delete", (req, res) => {
+    movie.findByIdAndDelete(req.params.id)
+        .then(result => {
+            console.log(req.params.id);
+            res.redirect('/favourites')
+        })
+        .catch(err => console.log(err))
+})
 
 
     // https://api.themoviedb.org/3/movie/week?api_key=f9c8e3bf33252c8c1179e8b6ca4946f1
